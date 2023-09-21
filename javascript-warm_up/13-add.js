8bab67c290510197d5e101849bbc690ffeb30e2d
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-21 20:15:30 +0100

    4. Add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // Define a function that takes 2 parameters and returns their sum
-function add(a, b) {
+//function add(a, b) {
     // Return the result of adding a and b
-    return a + b;
-  }
+  //  return a + b;
+  //}
   
   // The function is visible from outside and can be called with any 2 integers
-  console.log(add(3, 5)); // Prints 8
-  console.log(add(-2, 7)); // Prints 5
-  
+  //console.log(add(3, 5)); // Prints 8
+  //console.log(add(-2, 7)); // Prints 5
+  const add = require('./13-add').add;
+console.log(add(4, 85));
