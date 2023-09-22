7a3ac89a4781e15d8c492733e2b8469295dd6248
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-22 23:11:46 +0100

    4. Add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
 // This is a function that returns the addition of two integers
-// The function is visible from outside
-// The name of the function is add
-// The function does not use var
 
-function add(a, b) {
-  // Check if the arguments are integers
-  if (Number.isInteger(a) && Number.isInteger(b)) {
-    // Return the sum of the arguments
-    return a + b;
-  } else {
-    // Throw an error if the arguments are not integers
-    throw new Error("The arguments must be integers");
-  }
-}
-
+exports.add = function (a, b) {
+  const sum = a + b;
+  return sum;
+};
