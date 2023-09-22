f1a8c1f0e441404d0537f897a75648264dd9fbf5
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-22 23:32:43 +0100

    6. Call me Moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 // This is a function that executes x times a function
 
-function repeat(x, theFunction) {
-    // Check if the first argument is a positive integer
-    if (Number.isInteger(x) && x > 0) {
-      // Loop x times
-      for (let i = 0; i < x; i++) {
-        // Call the second argument as a function
-        theFunction();
-      }
-    } else {
-      // Throw an error if the first argument is not a positive integer
-      throw new Error("The first argument must be a positive integer");
+exports.callMeMoby = function (x, theFunction) {
+    for (let i = 1; i <= x; i++) {
+      theFunction();
     }
-  }
-    
+  };    
