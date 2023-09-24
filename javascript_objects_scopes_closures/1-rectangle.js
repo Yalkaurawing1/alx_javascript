ad0be7af0908cfc20754695e24d78b306ec9196b
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-24 23:47:27 +0100

    1. Rectangle #1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Define a class Rectangle
+class Rectangle {
+
+    // initialize an instance with these values
+    constructor (w, h) {
+        // constroctor with w and h paramiters
+        this.width = w;
+        this.height = h;
+    }
+}
+
+module.exports = Rectangle;
