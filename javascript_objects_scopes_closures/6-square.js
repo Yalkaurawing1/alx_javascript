5287891727abe0fb2d8b9c112e4689cc87c314d1
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-25 00:53:50 +0100

    6. Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,25 +4,25 @@
 const Square = require('./5-square');
 
 // Define a class Square that inherits from Square
-class Square extends Square {
-  // Define an instance method called charPrint
-  constructor (size) {
-    super(size, size);
-}
+class Square extends Square_ {
 
-charPrint(c) {
-    if (c) {
-        for (let i = 1; i <= this.height; i++) {
-            const rectanglr_c = c.repeat(this.width);
-            console.log(rectanglr_c);
-        }
-    } else {
-        for (let i = 1; i <= this.height; i++) {
-            const rectanglr_X = 'X'.repeat(this.width);
-            console.log(rectanglr_X);
+    constructor (size) {
+        super(size, size);
+    }
+
+    charPrint(c) {
+        if (c) {
+            for (let i = 1; i <= this.height; i++) {
+                const rectanglr_c = c.repeat(this.width);
+                console.log(rectanglr_c);
+            }
+        } else {
+            for (let i = 1; i <= this.height; i++) {
+                const rectanglr_X = 'X'.repeat(this.width);
+                console.log(rectanglr_X);
+            }
         }
     }
-}
 }
 
 module.exports = Square;
