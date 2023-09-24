eae70c91bba3986415d2488421929ea46a0958f0
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-24 23:42:33 +0100

    0. Rectangle #0

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -2,11 +2,7 @@
 // Define a class Rectangle
 class Rectangle {
     // Define the constructor method
-    constructor() {
-      // Use the pass statement to indicate that the constructor is empty
-      // You can add any parameters or properties here later
-      pass;
-    }
-    // You can add any other methods or properties for the class here
-  }
-  
+    constructor() {}
+}
+
+module.exports = Rectangle;
