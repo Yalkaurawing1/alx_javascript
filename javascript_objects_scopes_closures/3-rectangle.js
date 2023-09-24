2a3a1cca8f037818fb7389e8b82bd40934b48a01
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-25 00:11:54 +0100

    3. Rectangle #3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+// Define a class Rectangle
+class Rectangle {
+    // Define the constructor method
+    constructor(w, h) {
+      // Check if w and h are positive integers
+      if (w && h > 0) {
+        // Initialize the instance attribute width with the value of w
+        this.width = w;
+        // Initialize the instance attribute height with the value of h
+        this.height = h;
+      } else {
+        // Create an empty object
+        return 'Rectangle {}';
+      }
+    }
+    // Define an instance method called print
+    print() {
+      // Loop through the height of the rectangle
+      for (let i = 0; i < this.height; i++) {
+        const rectanglr_X = 'X'.repeat(this.width); 
+        console.log(x);
+      }
+
+    }
+
+}
+
+  
