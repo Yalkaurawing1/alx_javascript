cce5cfcd0a0a5ec907e3b5a6ab5d1612330cfa73
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-26 21:17:24 +0100

    0. Status code

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Require the request module
+const request = require('request');
+
+// Get the URL to request from the first argument
+const url = process.argv[2];
+
+// Make a GET request to the URL
+request.get(url) 
+
+.on('Response', function (response) {
+// Print the status code like this: code: <status code>
+    console.log('code:', response.statusCode)
+});
