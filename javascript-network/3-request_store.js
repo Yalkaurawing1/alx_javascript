1ccc6f03635667b733301b4a88817b1cee4bebcc
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-26 22:25:03 +0100

    3. Loripsum

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -2,33 +2,11 @@
 
 // Require the request module
 const request = require('request');
+const fs = require ('fs');
 
-// Get the URL to request from the first argument
-const url = process.argv[2];
+//argumets on command line
+url = process.argv[2];
+file_name = process.argv[3];
 
-// Get the file path to store the body response from the second argument
-const filePath = process.argv[3];
-
-// Require the fs module to write to the file
-const fs = require('fs');
-
-// Make a GET request to the URL
-request.get(url, (err, res, body) => {
-  // Check if there is an error
-  if (err) {
-    // Print the error message
-    console.error(err.message);
-  } else {
-    // Write the body response to the file using UTF-8 encoding
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-      // Check if there is an error
-      if (err) {
-        // Print the error message
-        console.error(err.message);
-      } else {
-        // Print a success message
-        console.log(`The contents of ${url} have been saved to ${filePath}`);
-      }
-    });
-  }
-});
+//make request
+request.get(url).pipe(fs.createWriteStream(file_name));
