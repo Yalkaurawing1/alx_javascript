906f961f7d482a1991dd6f80a06232c8ad5c69cc
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-27 23:17:29 +0100

    4. Complex Closure

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -32,37 +32,4 @@ function divideBy(firstNumber) {
   
   // divideBy100, that uses the function divideBy with the number 100
   let divideBy100 = divideBy(100);
-// Create a function divideBy that:
-function divideBy(firstNumber) {
-    // It takes into argument firstNumber (number)
-    // It returns a function that takes into argument secondNumber (number)
-    return function (secondNumber) {
-      // It returns the second number divided by the first number
-      return secondNumber / firstNumber;
-    };
-  }
   
-  // Create a function addBy that:
-  function addBy(firstNumber) {
-    // It takes into argument firstNumber (number)
-    // It returns a function that takes into argument secondNumber (number)
-    return function (secondNumber) {
-      // It returns the sum of the two numbers
-      return firstNumber + secondNumber;
-    };
-  }
-  
-  // Create four closures:
-  
-  // addBy100, that uses the function addBy with the number 100
-  let addBy100 = addBy(100);
-  
-  // addBy1000, that uses the function addBy with the number 1000
-  let addBy1000 = addBy(1000);
-  
-  // divideBy10, that uses the function divideBy with the number 10
-  let divideBy10 = divideBy(10);
-  
-  // divideBy100, that uses the function divideBy with the number 100
-  let divideBy100 = divideBy(100);
-    
