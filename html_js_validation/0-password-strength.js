fd25247217a7d0e6f96d8aaf4e88649ac8cb3902
Yusuf sani <yalkaurawing@yahoo.com>
2023-10-03 22:46:56 +0100

    0. Form Validation - Password Strength

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -1,57 +1,37 @@
-// Function to validate the password
-function validatePassword() {
-    const passwordInput = document.getElementById("password");
-    const errorParagraph = document.getElementById("error");
-  
-    // Regular expressions for each criteria
-    const minLength = 8;
-    const uppercaseRegex = /[A-Z]/;
-    const lowercaseRegex = /[a-z]/;
-    const digitRegex = /\d/;
-    const specialCharRegex = /[!@#$%^&*]/;
-  
-    const password = passwordInput.value;
-  
-    if (password.length < minLength) {
-      errorParagraph.textContent = "Password must be at least 8 characters long.";
-      errorParagraph.style.color = "red";
-      return false;
-    }
-  
-    if (!uppercaseRegex.test(password)) {
-      errorParagraph.textContent = "Password must contain at least one uppercase letter.";
-      errorParagraph.style.color = "red";
-      return false;
-    }
-  
-    if (!lowercaseRegex.test(password)) {
-      errorParagraph.textContent = "Password must contain at least one lowercase letter.";
-      errorParagraph.style.color = "red";
-      return false;
-    }
-  
-    if (!digitRegex.test(password)) {
-      errorParagraph.textContent = "Password must contain at least one numeric digit.";
-      errorParagraph.style.color = "red";
-      return false;
-    }
-  
-    if (!specialCharRegex.test(password)) {
-      errorParagraph.textContent = "Password must contain at least one special character (!@#$%^&*).";
-      errorParagraph.style.color = "red";
-      return false;
-    }
-  
-    // If all criteria are met, clear the error message
-    errorParagraph.textContent = "";
-    return true;
+// Get the password input element by its id
+var passwordInput = document.getElementById("password");
+
+// Get the error paragraph element by its id
+var error = document.getElementById("error");
+
+// Get the form element by its id
+var form = document.getElementById("passwordForm");
+
+// Define a function to validate the password based on the criteria
+function validatePassword(password) {
+  // Use regular expressions to test the password criteria
+  var lengthRegex = /.{8,}/; // At least 8 characters
+  var upperRegex = /[A-Z]/; // At least one uppercase letter
+  var lowerRegex = /[a-z]/; // At least one lowercase letter
+  var digitRegex = /[0-9]/; // At least one numeric digit
+  var specialRegex = /[^A-Za-z0-9]/; // At least one special character
+
+  // Return true if all the criteria are met, false otherwise
+  return lengthRegex.test(password) && upperRegex.test(password) && lowerRegex.test(password) && digitRegex.test(password) && specialRegex.test(password);
+}
+
+// Add an event listener to the form submit event
+form.addEventListener("submit", function(event) {
+  // Get the value of the password input
+  var password = passwordInput.value;
+
+  // Check if the password is valid
+  if (validatePassword(password)) {
+    // If valid, allow the form submission
+    alert("Form submitted successfully.");
+  } else {
+    // If invalid, display the error message and prevent the form submission
+    error.textContent = "Your password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one numeric digit, and one special character.";
+    event.preventDefault();
   }
-  
-  // Add an event listener to the form submission
-  const passwordForm = document.getElementById("passwordForm");
-  passwordForm.addEventListener("submit", function (event) {
-    if (!validatePassword()) {
-      event.preventDefault(); // Prevent form submission if validation fails
-    }
-  });
-  
+});
